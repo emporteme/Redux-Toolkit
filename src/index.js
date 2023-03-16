d2cd8dde57c624ad4304ac3c0af5f9bfe2ca19ee
emporteme <magzhankarataev02@gmail.com>
2023-03-16 12:11:37 +0600

    Main index.js file was wrapped by Provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { store } from './store'
+import { Ptovider } from 'react-redux'
+
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Ptovider store={store}>
+            <App />
+        </Ptovider>
     </React.StrictMode>,
     document.getElementById('root'),
 )
